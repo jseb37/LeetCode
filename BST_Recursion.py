87a30da23b87d06daad273c61f737dbf28d5a5e4
Jerin Sebastian <jseb3723@gmail.com>
2024-01-20 17:01:42 +0530

    BST_RecursionV1

--- BST_Recursion.py
+++ BST_Recursion.py
@@ -8,4 +8,12 @@ class BST:
         if root == None:
             return Node(ele)
         elif ele<root.data:
-            root.left=
+            root.left=self.buildBst(root.left,ele)
+        elif ele>root.data:
+            root.right=self.buildBst(root.right,ele)
+        return root
+
+root = None
+b = BST()
+for ele in [10,5,25,2,7,30]:
+    root = b.buildBst(root,ele)
