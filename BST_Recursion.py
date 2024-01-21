cecfb2b8e3050775bdac1b7b29866368920d450a
Jerin Sebastian <jseb3723@gmail.com>
2024-01-21 12:30:01 +0530

    bst_min_max_fn_added

--- BST_Recursion.py
+++ BST_Recursion.py
@@ -12,8 +12,19 @@ class BST:
         else:
             root.right=self.buildBst(root.right,ele)
         return root
-
+    def mini(self,root):
+        if root.left == None:
+           return root.data
+        return self.mini(root.left)
+    def maxim(self,root):
+       if root.right == None:
+          return root.data
+       return self.maxim(root.right)
 root = None
 b = BST()
 for ele in [10,5,25,2,7,30]:
     root = b.buildBst(root,ele)
+m=b.mini(root)
+print("Min value is {}".format(m))
+ma=b.maxim(root)
+print("Max value is {}".format(ma))
