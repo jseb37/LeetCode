0a826cdd8e1d013c2613dc4c543dd1899765cbe5
Jerin Sebastian <jseb3723@gmail.com>
2024-01-23 09:30:46 +0530

    add_bst_preordertraversal

---
+++ BST_PreorderTraversal.py
@@ -0,0 +1,28 @@
+#Preorder Traversal - RootNode,LeftNode,RightNode
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right= None
+class BST:
+    def buildBst(self,root,ele):
+        if root == None:
+            return Node(ele)
+        if ele<root.data:
+            root.left=self.buildBst(root.left,ele)
+        else:
+            root.right=self.buildBst(root.right,ele)
+        return root
+
+    def preorder(self,root):
+        if root == None:
+            return
+        print(root.data)
+        self.preorder(root.left)
+        self.preorder(root.right)
+
+root = None
+b = BST()
+for ele in [10,5,25,2,7,30]:
+    root = b.buildBst(root,ele)
+b.preorder(root)
