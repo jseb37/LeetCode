21386adbea4d1b5f7276ba8fb76af23934e0dfa6
Jerin Sebastian <jseb3723@gmail.com>
2024-02-27 17:36:11 +0530

    add_quicksort

---
+++ QuickSort.py
@@ -0,0 +1,9 @@
+'''
+Quick Sort Algorithm
+ Quick Sort An n-element Array
+1. Divide:Partition the array into two subarrays around a pivot x such that elements in lower subarray <=x<= elements in upper subarray
+2. Conquer:Recursively sort the two subarrays
+3. Combine:Trivial
+
+Key:Linear time partitioning subroutine
+'''
