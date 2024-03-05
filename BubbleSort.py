b2558bec8c36b1641806f7c6556415e5c1b0454d
Jerin Sebastian <jseb3723@gmail.com>
2024-03-05 08:40:11 +0530

    optimized_bubblesort

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,14 +1,22 @@
 def bubble_sort(arr):
     n = len(arr)
+    # optimize code, so if the array is already sorted, it doesn't need
+    # to go through the entire process
+    swapped = False
     # Traverse through all array elements
-    for i in range(n):
+    for i in range(n-1):
         # Last i elements are already in place
         for j in range(n-i-1):
             # Traverse the array from 0 to n-i-1
             # Swap if the element found is greater
             # than the next element
             if arr[j] > arr[j+1]:
+                swapped = True
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+        if not swapped:
+            # if we haven't needed to make a single swap, we
+            # can just exit the main loop.
+            return
 
 # Example usage:
 arr = [64, 34, 25, 12, 22, 11, 90]
