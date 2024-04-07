5e6047e6c06d91518ad5abaf3b8e23d4ec8c47f7
Jerin Sebastian <jseb3723@gmail.com>
2024-04-07 19:06:01 +0530

    add_search_2d_matrix.py

--- search_2d_matrix.py
+++ search_2d_matrix.py
@@ -30,7 +30,6 @@ class Solution(object):
                 break
         if not(top <= bot):
             return False
-        row = (top + bot) // 2
         l,r = 0 , COLS -1
         while l <= r:
             m = (l + r) //2
