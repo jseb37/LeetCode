4663c816fdd91a4df355ad04b4c0d19b2c61bb4d
Jerin Sebastian <jseb3723@gmail.com>
2024-06-03 09:38:41 +0530

    variable_name_modifications

--- Binary_Search.py
+++ Binary_Search.py
@@ -2,26 +2,20 @@
 # It returns index of n in given list1 if present,
 # else returns -1
 def binary_search(list1, n):
-    low = 0
-    high = len(list1) - 1
-    mid = 0
-
-    while low <= high:
+    left = 0
+    right = len(list1) - 1
+    while left <= right:
         # for get integer result
-        mid = (high + low) // 2
-
+        mid = (left + right) // 2
         # Check if n is present at mid
         if list1[mid] < n:
-            low = mid + 1
-
+            left = mid + 1
             # If n is greater, compare to the right of mid
         elif list1[mid] > n:
-            high = mid - 1
-
+            right = mid - 1
             # If n is smaller, compared to the left of mid
         else:
             return mid
-
             # element was not present in the list, return -1
     return -1
 
