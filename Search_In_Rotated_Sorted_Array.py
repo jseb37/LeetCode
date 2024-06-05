a9d0b49204f1818d39da65537db7a1018fde9751
Jerin Sebastian <jseb3723@gmail.com>
2024-06-05 08:11:01 +0530

    add_Search_In_Rotated_Sorted_Array

--- Search_In_Rotated_Sorted_Array.py
+++ Search_In_Rotated_Sorted_Array.py
@@ -38,28 +38,28 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        l = 0
-        r = len(nums) - 1
+        left = 0
+        right = len(nums) - 1
 
-        while l <= r:
-            mid = (l + r) // 2
+        while left <= right:
+            mid = (left + right) // 2
 
             if nums[mid] == target:
                 return mid
 
             # left sorted portion
-            elif nums[mid] >= nums[l]:
-                if nums[l] <= target <= nums[mid]:
-                    r = mid - 1
+            elif nums[mid] >= nums[left]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
                 else:
-                    l = mid + 1
+                    left = mid + 1
 
             # right sorted portion
-            else:
-                if nums[mid] <= target <= nums[r]:
-                    l = mid + 1
+            elif nums[mid] <= nums[left]:
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
                 else:
-                    r = mid - 1
+                    right = mid - 1
 
         return -1
 
