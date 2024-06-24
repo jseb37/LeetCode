6ee5eba9a0c85a07d4f9ecd444d605e2a7f2f8db
Jerin Sebastian <jseb3723@gmail.com>
2024-06-24 09:58:43 +0530

    added_comments

--- Group_Anagarams.py
+++ Group_Anagarams.py
@@ -34,10 +34,21 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        dic = defaultdict(list)
+        groupAnagramsdic = defaultdict(list)
+        print(groupAnagramsdic)
+
         for word in strs:
-            dic["".join(sorted(word))].append(word)
-        return dic.values()
+            #Key of below groupAnagramsdic will be the sorted word , which can be used to append anagram words
+            #Why using join?? - If u dont convert this to string via join it will give below error
+            #TypeError: unhashable type: 'list'
+            #The “TypeError: unhashable type: 'list'” error is raised when you try to assign a list as a key in a dictionary
+            #To solve this error, ensure you only assign a hashable object, such as a string or a tuple, as a key for a dictionary
+            #groupAnagramsdic[sorted(word)].append(word)
+
+
+            groupAnagramsdic["".join(sorted(word))].append(word)
+        print(groupAnagramsdic)
+        return groupAnagramsdic.values()
 
 
 s = Solution()
