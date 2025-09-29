b190f701ac451b82339fc446db40b281fa18b42c
Jerin Sebastian <jseb3723@gmail.com>
2025-09-29 22:14:43 +0530

    Update Unique_Email_Address.py

--- Unique_Email_Address.py
+++ Unique_Email_Address.py
@@ -48,5 +48,26 @@ class Solution:
         return len(unique)        
                 
 
+#Without inbuilt functions
+
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        unique = set()
+        for e in emails:
+            i, local = 0,""
+            while e[i] not in ["@","+"]:
+                if e[i] != ".":
+                    local += e[i]
+                i += 1
+
+            while e[i] != "@":
+                i += 1
+
+            domain = e[i+1:]     
+            unique.add((local,domain))
+
+        return len(unique)        
+                
+
 
 
