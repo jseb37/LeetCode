eb92b0c525830ada6b998d43585193ed2753e0ee
Jerin Sebastian <jseb3723@gmail.com>
2025-10-04 11:32:53 +0530

    Update Longest_StrictlyIncreasing_Or_DecreasingSubArray.py

--- Longest_StrictlyIncreasing_Or_DecreasingSubArray.py
+++ Longest_StrictlyIncreasing_Or_DecreasingSubArray.py
@@ -60,10 +60,13 @@ class Solution:
         res = 1
 
         for i in range(1, len(nums)):
+         
             if nums[i] == nums[i - 1]:
                 inc = dec = 1
+             
             elif nums[i] > nums[i - 1]:
                 inc, dec = inc + 1, 1
+             
             else:
                 inc, dec = 1, dec + 1
 
